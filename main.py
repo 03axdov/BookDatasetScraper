461ffaa6e9e4ee96453d56dc0036f67257b76c29
axeld <axel.dovskog@outlook.com>
2022-11-04 22:49:13 +0100

    Utility

--- main.py
+++ main.py
@@ -6,28 +6,50 @@ import numpy as np
 from bs4 import BeautifulSoup
 import requests
 
-from utils import *
+from utils import process_num_ratings, create_ds, write_to_file, handle_length
+
+FILE_PATH = "db.txt"
+
+def main():
+    count = 0
+    index = 1049000
+    while True:
+        URL = "https://www.imdb.com/title/tt" + "0" * (7 - len(str(index))) + str(index) + "/"
+
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        try:
+            storyline = soup.find("span", {"class": "sc-16ede01-1 kgphFu"}).text
+            num_ratings = soup.find("div", {"class": "sc-7ab21ed2-3 dPVcnq"}).text
+            rating = soup.find("span", {"class": "sc-7ab21ed2-1 jGRxWM"}).text
+            title = soup.find("h1", {"class", "sc-b73cd867-0 eKrKux"}).text
+            year = soup.find("a", {"class": "ipc-link ipc-link--baseAlt ipc-link--inherit-color sc-8c396aa2-1 WIUyh"}).text
+
+            list = soup.find("ul", {"class": "ipc-inline-list ipc-inline-list--show-dividers sc-8c396aa2-0 kqWovI baseAlt"})
+            try:
+                length = list.findChildren("li")[2].text
+            except IndexError:
+                length = "error"    # Will become -1
+
+            if storyline and rating and process_num_ratings(num_ratings) > 10000 and len(storyline) > 125 and len(year) == 4 and int(year) > 1945:
+                print("")
+                print(storyline)
+                print(rating)
+                print("")
+                print(f"https://www.imdb.com/title/tt{'0' * (7 - len(str(index))) + str(index) + '/'}")
+                count += 1
+
+                write_to_file(rating, storyline, title, year, length, FILE_PATH)
+
+            index += 1
+        except AttributeError:
+            index += 1
+
+        if index % 25 == 0:
+            print(f"Processing - {index}")
 
 
-count = 0
-index = 135852
-while count < 5:
-    URL = "https://www.imdb.com/title/tt" + "0" * (7 - len(str(index))) + str(index) + "/"
 
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-
-    try:
-        storyline = soup.find("span", {"class": "sc-16ede01-1 kgphFu"})
-        num_ratings = soup.find("div", {"class": "sc-7ab21ed2-3 dPVcnq"})
-        rating = soup.find("span", {"class": "sc-7ab21ed2-1 jGRxWM"})
-        if storyline.text and rating.text and process_num_ratings(num_ratings.text) > 10000:
-            print("")
-            print(storyline.text)
-            print(rating.text)
-            print("")
-            print(f"https://www.imdb.com/title/tt{'0' * (7 - len(str(index))) + str(index) + '/'}")
-            count += 1
-        index += 1
-    except AttributeError:
-        index += 1
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -5,3 +5,49 @@ def process_num_ratings(rating):
         return float(rating[0: len(rating) - 1]) * 1000000
     else:
         return float(rating)
+
+
+def create_ds(file_path = "db.txt"):
+    labels = []
+    text = []
+    titles = []
+    years = []
+    lengths = []
+
+    with open(file_path) as file:
+        for line in file:
+            line_list = line.split(" <SEP> ")
+
+            labels.append(line_list[0])
+            text.append(line_list[1])
+            titles.append(line_list[2])
+            years.append(line_list[3])
+            lengths.append(line_list[4].replace("\n", ""))
+
+        file.close()
+
+    return labels, text, titles, years, lengths
+
+
+def write_to_file(rating, text, title, year, length, file_path = "db.txt"):
+    with open(file_path, 'a') as file:
+        file.write(str(rating) + " <SEP> " + str(text) + " <SEP> " + str(title) + " <SEP> " + str(year) + " <SEP> " + str(handle_length(length)) + "\n")
+
+        file.close()
+
+
+def handle_length(length):  # Minutes
+    length = length.replace(" ", "")
+
+    time = 0
+    current = ''
+    for i in length:
+        if i.lower() != "h" and i.lower() != "m":
+            current += i
+        elif i.lower() == "h":
+            time += int(current) * 60
+            current = ''
+        else:
+            time += int(current)
+
+    return time
